57d088de2aca02a0ad20f923d14420d67938891c
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2023-11-26 19:32:29 +0300

    fix: publication issues with jitpack.io

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,3 +18,8 @@ subprojects {
         maven { url = uri("https://jitpack.io") }
     }
 }
+
+// https://github.com/jitpack/jitpack.io/issues/3853#issuecomment-1683838845
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
+}
